118e6b33ce7f7f5ceeb71454a6d1b56c69f1cc7b
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2022-02-09 06:00:14 -0800

    [Push To Joe Branch] Disabled Idle Script
    - Disabled the idle script function that was causing a lot of error in main.

--- Assets/Scripts/AI/idleMoveAI.cs
+++ Assets/Scripts/AI/idleMoveAI.cs
@@ -1,48 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.AI;
 
-public class idleMoveAI : MonoBehaviour
-{
-    public Transform[] directPoints;
-    private int index = 0;
-    public float patroTime = 2f;//wait time
+//public class idleMoveAI : MonoBehaviour
+//{
+//    public Transform[] directPoints;
+//    private int index = 0;
+//    public float patroTime = 2f;//wait time
 
-    private float timer = 0;//timer
-    private NavMeshAgent navMeshAgent;
+//    private float timer = 0;//timer
+//    private NavMeshAgent navMeshAgent;
 
-    void Start()
-    {
-        navMeshAgent = GetComponent<NavMeshAgent>();
+//    void Start()
+//    {
+//        navMeshAgent = GetComponent<NavMeshAgent>();
 
-        navMeshAgent.destination = directPoints[index].position;
+//        navMeshAgent.destination = directPoints[index].position;
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (navMeshAgent.remainingDistance < 1.5f)
-        {
-            timer += Time.deltaTime;
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (navMeshAgent.remainingDistance < 1.5f)
+//        {
+//            timer += Time.deltaTime;
 
-            if (timer >= patroTime)
-            {
-                index++;
+//            if (timer >= patroTime)
+//            {
+//                index++;
 
-                index %= 5;//5points
+//                index %= 5;//5points
 
-                timer = 0;
-                if (index>= directPoints.Length)
-                {
-                    index = 0;
-                }
-                navMeshAgent.destination = directPoints[index].position;
-            }
-        }
+//                timer = 0;
+//                if (index>= directPoints.Length)
+//                {
+//                    index = 0;
+//                }
+//                navMeshAgent.destination = directPoints[index].position;
+//            }
+//        }
 
-    }
-}
-
-
+//    }
+//}
