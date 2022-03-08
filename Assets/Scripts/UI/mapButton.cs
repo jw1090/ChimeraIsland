a8c70ef5f80a3a7388a03a34ac91d12465266a90
TheSolomonster <Aidan@Solomonster.org>
2022-03-07 19:53:14 -0800

    fixed transition
    -implemented everything from my previous versions but changed it to align with main

--- Assets/Scripts/UI/mapButton.cs
+++ Assets/Scripts/UI/mapButton.cs
@@ -1,30 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mapButton : MonoBehaviour
 {
     public Animator transition;
     [SerializeField] GameObject mapUI;
-    [SerializeField] GameObject gameUI;
+    [SerializeField] GameObject crossFade;
     public float transitionTime = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(crossfadeOff());
     }
     public void onClickPlains()
     {
-        transition.SetBool("Start", true);
+        crossFade.SetActive(true);
+        transition.SetBool("start", true);
         StartCoroutine(transitionP());
     }
     IEnumerator transitionP()
     {
         yield return new WaitForSeconds(transitionTime);
+        this.GetComponent<Image>().enabled = false;
         mapUI.SetActive(false);
-        gameUI.SetActive(true);
-        transition.SetBool("Start", false);
-
+        transition.SetBool("start", false); 
+        yield return new WaitForSeconds(transitionTime);
+        crossFade.SetActive(false);
+    }
+    IEnumerator crossfadeOff()
+    {
+        yield return new WaitForSeconds(transitionTime);
+        crossFade.SetActive(false);
     }
     // Update is called once per frame
     void Update()
