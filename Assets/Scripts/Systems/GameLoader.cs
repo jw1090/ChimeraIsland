7fccf8b055895d7f49cef5c0fa75fef0f80a9eed
PC-Craig\craig <craiglacey@gmail.com>
2022-04-13 01:18:14 -0700

    Adding some system init setup

---
+++ Assets/Scripts/Systems/GameLoader.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : AsyncLoader
+{
+    [SerializeField] private int sceneIndexToLoad = 1;
+    public List<Component> GameModules;
+
+    private static int _sceneIndex = 1;
+    private static GameLoader _instance;
+    
+	protected override void Awake()
+	{
+		Debug.Log("GameLoader Starting");
+
+		// Saftey check
+		if (_instance != null && _instance != this)
+		{
+			Debug.Log("A duplicate instance of the GameLoader was found, and will be ignored. Only one instance is permitted");
+			Destroy(gameObject);
+			return;
+		}
+
+		// Set reference to this instance
+		_instance = this;
+
+		// Make persistent
+		DontDestroyOnLoad(gameObject);
+
+        // Scene Index Check
+        if (sceneIndexToLoad < 0 || sceneIndexToLoad >= SceneManager.sceneCountInBuildSettings)
+        {
+            Debug.Log($"Invalid Scene Index {sceneIndexToLoad} ... using default value of {_sceneIndex}");
+        }
+        else
+        {
+            Debug.Log($"Scene index to load is set to {_sceneIndex}");
+            _sceneIndex = sceneIndexToLoad;
+        }
+
+		// Setup System GameObject
+		GameObject systemsGO = new GameObject("[Services]");
+		Transform systemsParent = systemsGO.transform;
+		DontDestroyOnLoad(systemsGO);
+
+        // Queue up loading routines
+        Enqueue(IntializeCoreSystems(systemsParent), 1);
+		Enqueue(InitializeModularSystems(systemsParent), 2);
+
+		// Set completion callback
+		CallOnComplete(OnComplete);
+	}
+
+	private IEnumerator IntializeCoreSystems(Transform systemsParent)
+	{
+        Debug.Log("Loading Core Systems");
+
+        //var screenManagerGO = Instantiate(screenManager, systemsParent);
+        //screenManagerGO.name = "ScreenManager";
+        //var screenManagerComp = screenManagerGO.GetComponent<ScreenManager>().Initialize();
+        //ServiceLocator.Register<ScreenManager>(screenManagerComp);
+
+		yield return null;
+	}
+
+	private IEnumerator InitializeModularSystems(Transform systemsParent)
+	{
+        // Setup Additional Systems as needed
+        Debug.Log("Loading Modular Systems");
+        foreach (Component c in GameModules)
+        {
+            if (c is IGameModule)
+            {
+                IGameModule module = (IGameModule)c;
+                yield return module.LoadModule();
+            }
+        }
+
+		yield return null;
+	}
+
+    private IEnumerator LoadInitialScene(int index)
+    {
+        Debug.Log($"GameLoader -> Starting Scene Load: {index}");        
+        yield return SceneManager.LoadSceneAsync(index);
+    }
+
+	// AsyncLoader completion callback
+	private void OnComplete()
+	{
+		Debug.Log("GameLoader Finished Initializing");
+        StartCoroutine(LoadInitialScene(_sceneIndex));
+    }
+}
---
+++ Assets/Scripts/Systems/ServiceLocator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+public static class ServiceLocator
+{
+    private static readonly Dictionary<System.Type, object> _systemRegistry = new Dictionary<System.Type, object>();
+
+    public static void Register<T>(object system)
+    {
+        if (!_systemRegistry.ContainsKey(typeof(T)))
+        {
+            _systemRegistry.Add(typeof(T), system);
+            return;
+        }
+    }
+
+    public static void Register(System.Type systemType, object system)
+    {
+        if (!_systemRegistry.ContainsKey(systemType))
+        {
+            _systemRegistry.Add(systemType, system);
+            return;
+        }
+    }
+
+    public static T Get<T>()
+    {
+        T result = default(T);
+
+        return result;
+    }
+
+    public static bool Contains<T>()
+    {
+        return _systemRegistry.ContainsKey(typeof(T));
+    }
+}
