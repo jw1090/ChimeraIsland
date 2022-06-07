942e4e83c46b42dade4be7b1694f4d3f1cb196c2
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2022-06-06 18:20:24 -0700

    [Joe Branch] Added Transfer Button Logic
    - Map now appears with its associated button in Details UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,9 +4,10 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private Marketplace _marketplace = null;
-    [SerializeField] private Button _openChimerasButton = null;
-    [SerializeField] private Button _closeChimerasButton = null;
+    [SerializeField] private Button _openDetailsButton = null;
+    [SerializeField] private Button _closeDetailsButton = null;
     [SerializeField] private ChimeraDetailsFolder _detailsFolder = null;
+    [SerializeField] private GameObject _transferMap = null;
     [SerializeField] private ReleaseSlider _releaseSlider = null;
     [SerializeField] private UITutorialOverlay _tutorialOverlay = null;
     [SerializeField] private UIWallet[] _essenceWallets = null;
@@ -49,10 +50,11 @@ public class UIManager : MonoBehaviour
 
     public void CloseAll()
     {
+        _openDetailsButton.gameObject.SetActive(true);
         _marketplace.gameObject.SetActive(false);
-        _openChimerasButton.gameObject.SetActive(true);
-        _closeChimerasButton.gameObject.SetActive(false);
+        _closeDetailsButton.gameObject.SetActive(false);
         _detailsFolder.gameObject.SetActive(false);
+        _transferMap.gameObject.SetActive(false);
     }
 
     public void OpenDetailsPanel()
@@ -60,8 +62,8 @@ public class UIManager : MonoBehaviour
         _detailsFolder.CheckDetails();
 
         CloseAll();
-        _openChimerasButton.gameObject.SetActive(false);
-        _closeChimerasButton.gameObject.SetActive(true);
+        _openDetailsButton.gameObject.SetActive(false);
+        _closeDetailsButton.gameObject.SetActive(true);
         _detailsFolder.gameObject.SetActive(true);
     }
 
@@ -69,6 +71,12 @@ public class UIManager : MonoBehaviour
     {
         CloseAll();
         _marketplace.gameObject.SetActive(true);
+    }
+
+    public void OpenTransferMap()
+    {
+        CloseAll();
+        _transferMap.gameObject.SetActive(true);
     }
 
     public void UpdateDetails()
