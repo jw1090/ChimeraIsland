7465533f4b3288ee777c32f7f27c0e4ffde8173c
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2022-06-09 00:13:20 -0700

    Update TutorialManager.cs

--- Assets/Scripts/UI/Tutorial/TutorialManager.cs
+++ Assets/Scripts/UI/Tutorial/TutorialManager.cs
@@ -4,10 +4,11 @@ using System;
 public class TutorialManager : MonoBehaviour
 {
     private UIManager _uiManager = null;
-    private UITutorialOverlay _tutorialOverlay = null; // TODO: Kill me
-    private Tutorial _tutorialData;
+    private Tutorial _tutorialData = null;
 
     public event Action OnTutorialComplete;
+
+    public void SetUIManager(UIManager uiManager) { _uiManager = uiManager; }
 
     public TutorialManager Initialize()
     {
@@ -15,31 +16,21 @@ public class TutorialManager : MonoBehaviour
         return this;
     }
 
-    public void SetUIManager(UIManager uiManager) 
-    {
-        _uiManager = uiManager;
-    }
-
     private void LoadTutorialFromJson()
     { 
         _tutorialData = FileHandler.ReadFromJSON<Tutorial>(GameConsts.JsonSaveKeys.TUTORIAL_DATA_FILE);
-        Debug.Log("<color=yelow> Tutorial Data Loaded</color>");
+        Debug.Log($"<color=yelow> Tutorial Data Loaded</color>");
     }
 
     public void ShowTutorial(int tutorialId)
     {
-        TutorialSteps tutorialSteps = GetTutorialData(tutorialId);
-        _uiManager.StartTutorial(tutorialSteps);
+        TutorialSteps tutorialStep = _tutorialData.Tutorials[tutorialId];
 
-        // UI elements should be managed by the UIManager - this logic can be done by the UIManager.
-        //_tutorialOverlay = _uiManager.TutorialOverlay;
-        //_tutorialOverlay.ShowOverlay();
-    }
+        if (tutorialStep == null)
+        {
+            Debug.LogError($"Tutorial result is null!");
+        }
 
-    private TutorialSteps GetTutorialData(int id)
-    {
-        var result = _tutorialData.Tutorials[id];
-        // error check that result is not null maybe
-        return result;
+        _uiManager.StartTutorial(tutorialStep);
     }
 }
