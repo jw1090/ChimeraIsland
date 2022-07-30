59bc4eecda33ee3f4e736c358da6093553fb9f97
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2022-07-29 20:48:27 -0700

    [Joe Branch] Fixed Bug with facilities spawning at level 2

--- Assets/Scripts/Systems/Lighting/LightRotate.cs
+++ Assets/Scripts/Systems/Lighting/LightRotate.cs
@@ -3,15 +3,9 @@ using UnityEngine;
 public class LightRotate : MonoBehaviour
 {
     [SerializeField] private float _speed = 1.0f;
-    private Vector3 _rotationVector = Vector3.zero;
-
-    private void Awake()
-    {
-        _rotationVector = new Vector3(0.0f, _speed, 0.0f);
-    }
 
     private void Update()
     {
-        this.transform.Rotate(_rotationVector * Time.deltaTime);
+        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
     }
 }
