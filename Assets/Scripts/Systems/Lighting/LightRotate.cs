e7ff6789a4cc131348d952c660c2d8a6e4966135
dextroaching <jupisantiagoks@hotmail.com>
2022-08-08 17:36:01 -0700

    [Santiago Branch] Light rotation
    - Work in progress

--- Assets/Scripts/Systems/Lighting/LightRotate.cs
+++ Assets/Scripts/Systems/Lighting/LightRotate.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 public class LightRotate : MonoBehaviour
 {
     [SerializeField] private float _speed = 1.0f;
+    [SerializeField] private DayType _dayType = DayType.None;
+    [SerializeField] private Quaternion _nightStart;
 
     private void Update()
     {
-        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+        float angle = _speed * Time.deltaTime;
+        transform.Rotate(Vector3.up, angle);
+        Debug.Log(Quaternion.Angle(gameObject.transform.rotation, _nightStart));
+        if(gameObject.transform.rotation == _nightStart)
+        {
+            
+            Debug.Log("crazy");
+        }
     }
 }
