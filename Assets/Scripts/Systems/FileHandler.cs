8bf4a9bb963746d6c0497614777b70c4ee4a3969
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2022-08-19 21:30:08 -0700

    [Joe Branch] Fixed Directory Issues When Saving

--- Assets/Scripts/Systems/FileHandler.cs
+++ Assets/Scripts/Systems/FileHandler.cs
@@ -2,34 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 
 public static class FileHandler
 {
-    public static void SaveToJSON<T>(List<T> toSave, string filename)
-    {
-        Debug.Log(GetPath(filename));
-        string content = JsonHelper.ToJson<T>(toSave.ToArray(), true);
-        WriteFile(GetPath(filename), content);
-    }
-
     public static void SaveToJSON<T>(T toSave, string filename)
     {
         string content = JsonUtility.ToJson(toSave, true);
         WriteFile(GetPath(filename), content);
-    }
-
-    public static List<T> ReadListFromJSON<T>(string filename)
-    {
-        string content = ReadFile(GetPath(filename));
-
-        if (string.IsNullOrEmpty(content) || content == "{}")
-        {
-            return new List<T>();
-        }
-
-        List<T> res = JsonHelper.FromJson<T>(content).ToList();
-        return res;
     }
 
     public static T ReadFromJSON<T>(string filename)
@@ -56,6 +37,13 @@ public static class FileHandler
 
     private static void WriteFile(string path, string content)
     {
+        string directory = Path.GetDirectoryName(path);
+
+        if (Directory.Exists(directory) == false)
+        {
+            Directory.CreateDirectory(directory);
+        }
+
         FileStream fileStream = new FileStream(path, FileMode.Create);
 
         using (StreamWriter writer = new StreamWriter(fileStream))
@@ -75,34 +63,5 @@ public static class FileHandler
             }
         }
         return "";
-    }
-}
-
-public static class JsonHelper
-{
-    public static T[] FromJson<T>(string json)
-    {
-        Wrapper<T> wrapper = JsonUtility.FromJson<Wrapper<T>>(json);
-        return wrapper.Items;
-    }
-
-    public static string ToJson<T>(T[] array)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper, true);
-    }
-
-    public static string ToJson<T>(T[] array, bool prettyPrint)
-    {
-        Wrapper<T> wrapper = new Wrapper<T>();
-        wrapper.Items = array;
-        return JsonUtility.ToJson(wrapper, prettyPrint);
-    }
-
-    [Serializable]
-    private class Wrapper<T>
-    {
-        public T[] Items;
     }
 }
