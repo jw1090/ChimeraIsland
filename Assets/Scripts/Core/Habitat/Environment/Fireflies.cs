88331415814cbaab4ce10fa46636c7db9948ede6
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-01-14 23:19:16 -0800

    [Santiago Branch] Firefly Light Source Fade

--- Assets/Scripts/Core/Habitat/Environment/Fireflies.cs
+++ Assets/Scripts/Core/Habitat/Environment/Fireflies.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Fireflies : MonoBehaviour
@@ -15,12 +16,49 @@ public class Fireflies : MonoBehaviour
     public void PlayFireflies()
     {
         _module.enabled = true;
-        _light.gameObject.SetActive(true);
+        StartCoroutine(FireFlyFade(true));
     }
 
     public void StopFireflies()
     {
         _module.enabled = false;
-        _light.gameObject.SetActive(false);
+        StartCoroutine(FireFlyFade(false));
+    }
+
+    private IEnumerator FireFlyFade(bool fadeIn)
+    {
+        float timer = 0.0f;
+        float duration = 5.0f;
+        float startingIntensity = _light.intensity;
+
+        if (fadeIn == true)
+        {
+            _light.gameObject.SetActive(true);
+        }
+
+        while (timer < duration)
+        {
+            timer += Time.deltaTime;
+
+            float progress = timer / duration;
+
+            float newIntensity;
+            if (fadeIn == true)
+            {
+                newIntensity = Mathf.Lerp(startingIntensity, 40.0f, progress);
+            }
+            else
+            {
+                newIntensity = Mathf.Lerp(startingIntensity, 0.0f, progress);
+            }
+            _light.intensity = newIntensity;
+
+            yield return null;
+        }
+
+        if (fadeIn == false)
+        {
+            _light.gameObject.SetActive(false);
+        }
     }
 }
