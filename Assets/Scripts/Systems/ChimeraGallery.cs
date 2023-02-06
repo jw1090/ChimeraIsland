b7b0144333c31be053ac561339e378d260566451
TheSolomonster <Aidan@Solomonster.org>
2023-02-06 15:27:57 -0800

    [Aidan Branch] Improve Chimera Gallery
    - Added Chimera Info UI to Gallery UI
    - You can now spin Chimera left and right in gallery view by clicking and dragging mouse
    - Added different camera nodes depending on size of chimera so they're never out of frame

--- Assets/Scripts/Systems/ChimeraGallery.cs
+++ Assets/Scripts/Systems/ChimeraGallery.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ChimeraGallery : MonoBehaviour
@@ -6,7 +7,9 @@ public class ChimeraGallery : MonoBehaviour
 
     [Header("Camera Nodes")]
     [SerializeField] private Transform _cameraTempleNode = null;
-    [SerializeField] private Transform _cameraGalleryNode = null;
+    [SerializeField] private Transform _galleryNodeSmall = null;
+    [SerializeField] private Transform _galleryNodeMedium = null;
+    [SerializeField] private Transform _galleryNodeLarge = null;
 
     [Header("ChimeraModels")]
     [SerializeField] private GameObject _chimeraA = null;
@@ -22,12 +25,17 @@ public class ChimeraGallery : MonoBehaviour
     [SerializeField] private GameObject _chimeraC2 = null;
     [SerializeField] private GameObject _chimeraC3 = null;
 
+    private StartingChimeraInfo _chimeraInfo = null;
+    private bool _active = false;
     private TempleUI _templeUI;
     private GameObject _currentChimera = null;
+    private Vector3 mPrevMousePos = Vector3.zero;
+    private Vector3 mPosDelta = Vector3.zero;
 
     public void Initialize()
     {
-
+        _chimeraInfo = ServiceLocator.Get<UIManager>().TempleUI.ChimeraInfo;
+        _chimeraInfo.Initialize();
     }
 
     public void SetTempleUI(TempleUI templeUI)
@@ -35,21 +43,55 @@ public class ChimeraGallery : MonoBehaviour
         _templeUI = templeUI;
     }
 
-    public void StartGallery(ChimeraType chimeraType)
+    public IEnumerator StartGallery(ChimeraType chimeraType)
     {
         _templeUI.ShowGalleryUIState();
         GetCurrentChimera(chimeraType);
         _currentChimera.SetActive(true);
-        _cameraUtil.gameObject.transform.position = _cameraGalleryNode.position;
-        _cameraUtil.gameObject.transform.rotation = _cameraGalleryNode.rotation;
+        switch (chimeraType)
+        {
+            case ChimeraType.A3:
+                _cameraUtil.gameObject.transform.position = _galleryNodeMedium.position;
+                _cameraUtil.gameObject.transform.rotation = _galleryNodeMedium.rotation;
+                break;
+            case ChimeraType.C2:
+            case ChimeraType.C3:
+                _cameraUtil.gameObject.transform.position = _galleryNodeLarge.position;
+                _cameraUtil.gameObject.transform.rotation = _galleryNodeLarge.rotation;
+                break;
+            default:
+                _cameraUtil.gameObject.transform.position = _galleryNodeSmall.position;
+                _cameraUtil.gameObject.transform.rotation = _galleryNodeSmall.rotation;
+                break;
+        }
+        _chimeraInfo.LoadChimeraData(_currentChimera.GetComponent<EvolutionLogic>());
+        yield return new WaitUntil(() => Input.GetMouseButtonUp(0) == true);
+        _active = true;
     }
 
 
     public void ExitGallery()
     {
+        _active = false;
+        _currentChimera.transform.localRotation = Quaternion.identity;
         _currentChimera.SetActive(false);
         _cameraUtil.gameObject.transform.position = _cameraTempleNode.position;
         _cameraUtil.gameObject.transform.rotation = _cameraTempleNode.rotation;
+    }
+
+    private void Update()
+    {
+        if (_active == false)
+        {
+            return;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            mPosDelta = Input.mousePosition - mPrevMousePos;
+            _currentChimera.transform.Rotate(_cameraUtil.gameObject.transform.up, -Vector3.Dot(mPosDelta, _cameraUtil.transform.right), Space.World);
+            _currentChimera.transform.localEulerAngles = new Vector3(0.0f, _currentChimera.transform.localEulerAngles.y, 0.0f);
+        }
+        mPrevMousePos = Input.mousePosition;
     }
 
     private void GetCurrentChimera(ChimeraType chimeraType)
