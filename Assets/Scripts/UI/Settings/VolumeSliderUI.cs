8ad62e8ed7bb6517b3d6c683206172de6f50bae7
Joseph Walden <79236028+jw1090@users.noreply.github.com>
2023-04-04 23:54:06 -0700

    [Joe Branch] Settings Slider UI

---
+++ Assets/Scripts/UI/Settings/VolumeSliderUI.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSliderUI : MonoBehaviour
+{
+    [SerializeField] Slider _slider = null;
+    [SerializeField] TextMeshProUGUI _text = null;
+
+    public Slider Slider { get => _slider; }
+    public TextMeshProUGUI Text { get => _text; }
+}
