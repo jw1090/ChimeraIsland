12fdf37ceb973e70bccef964fd0b2808b13c2e83
dextroaching <jupisantiagoks@hotmail.com>
2023-05-18 22:37:37 -0700

    [Santiago Branch] Quest Scriptable Object Created

---
+++ Assets/Scripts/Systems/Data/QuestBuilder/QuestData.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QuestData", menuName = "ScriptableObjects/Quest", order = 2)]
+public class QuestData : ScriptableObject
+{
+    public QuestType QuestType = QuestType.None;
+    public string Title = "";
+    [TextArea(3, 10)] public string Description = "";
+    public List<QuestType> QuestUnlocks = new List<QuestType>();
+}
